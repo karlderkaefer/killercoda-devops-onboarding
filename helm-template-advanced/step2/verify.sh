c81ddcc18e2912282ff9221276e4cdbce991964b
karlderkaefer <karlderkaefer@users.noreply.github.com>
2022-10-08 15:56:25 +0200

    add task 1

--- helm-template-advanced/step2/verify.sh
+++ helm-template-advanced/step2/verify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export RESULT_LOG="/tmp/results.log"
 rm -rf "${RESULT_LOG}"
-helm template /app >> "${RESULT_LOG}"
-helm conftest /app >> "${RESULT_LOG}"
+helm template /app 2>&1 | tee "${RESULT_LOG}"
+helm conftest /app 2>&1 | tee "${RESULT_LOG}"

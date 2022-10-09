b9ceb0862e327b3b96fbbcd13b36466be74f533e
karlderkaefer <karlderkaefer@users.noreply.github.com>
2022-10-09 16:59:32 +0200

    add task5

--- helm-template-basic/step5/verify.sh
+++ helm-template-basic/step5/verify.sh
@@ -8,8 +8,8 @@ rm -rf /tmp/results.log
 cd /app/step5
 
 num="$(helm template . | grep -o '\-\-' | wc -l)"
-if [[ "$num" != "5" ]]; then
-  echo "expected 5 yaml deployments objects. You need to add the sub-charts to dependencies." > /tmp/results.log
+if [[ "$num" != "4" ]]; then
+  echo "expected 4 yaml deployments objects. You need to add the sub-charts to dependencies." > /tmp/results.log
   exit 1
 fi
 

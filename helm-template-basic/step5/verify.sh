1e18e48851d64a02194ae0c7f2f13c63d7ac4843
karlderkaefer <karlderkaefer@users.noreply.github.com>
2022-10-09 17:15:20 +0200

    add task5

--- helm-template-basic/step5/verify.sh
+++ helm-template-basic/step5/verify.sh
@@ -9,7 +9,7 @@ cd /app/step5
 
 num="$(helm dep ls | wc -l)"
 if [[ "$num" != "4" ]]; then
-  helm plugin ls >> /tmp/results.log
+  helm dep ls >> /tmp/results.log
   echo "expected 3 helm dependencies. You need to add the sub-charts to dependencies." >> /tmp/results.log
   exit 1
 fi

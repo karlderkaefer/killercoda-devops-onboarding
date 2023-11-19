1c37b5bfffb23865c2c888d6803e6bd27c4d96c0
karlderkaefer <karlderkaefer@users.noreply.github.com>
2023-11-19 19:42:34 +0100

    add sql workshop

--- sql-basics/foreground.sh
+++ sql-basics/foreground.sh
@@ -1,10 +1,15 @@
-
 #!/bin/bash
 # This script sets up the basic SQL environment for the workshop
 
 # Install SQLite - a simple SQL database
 apt-get update
 apt-get install -y sqlite3
+
+# wait until sqlite3 is ready
+# wait until sqlite3 is ready
+until sqlite3 --version; do
+    sleep 1
+done
 
 # Create a sample database and table for the workshop
 sqlite3 workshop.db "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);"
